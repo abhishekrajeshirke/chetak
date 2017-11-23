035e0e50d5de1512b0940fb2284ce8a5e85a4403
Abhishek <abhishek.rajeshirke@wwindia.com>
2017-11-23 22:16:50 +0530

    First push for chetak frontend

---
+++ service.js
@@ -0,0 +1,30 @@
+app.service("appService", function($http, $q){
+
+  this.getData = function(){
+    var posts;
+    // create deferred object using $q
+    var deferred = $q.defer();
+    // var endpoint = 'http://localhost:1337/application';
+    endpoint = 'data.json';
+    // get posts form backend
+    $http.get(endpoint)
+     .then(function(result) {
+       // save fetched posts to the local variable
+       data = result.data;
+
+       // resolve the deferred
+       deferred.resolve(data);
+     }, function(error) {
+       posts = error;
+       deferred.reject(error);
+     });
+
+     // set the posts object to be a promise until result comeback
+      data = deferred.promise;
+
+      return $q.when(data);
+    }
+
+
+
+});
