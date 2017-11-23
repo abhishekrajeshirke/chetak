cf38916b62dab61a24ff4dd6c2f3df703dbad687
Abhishek <abhishek.rajeshirke@wwindia.com>
2017-11-23 23:08:06 +0530

    added server.js

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
-// var app = require("express")();
-//
-//
-// app.listen()
-//
-// app.get("/", function(req, res){
-//   res.sendFile("index.html");
-// });
+var app = require("express")();
+
+const path = require('path');
+
+// const bodyParser = require("body-parser");
+// const path = require("path");
+const app = express();
+
+const port = process.env.port || process.env.PORT || 1337;
+
+
+
+//Serving Static files
+app.use('/app.js',express.static(path.join(__dirname, '/app.js')));
+app.use('/assets',express.static(path.join(__dirname, '/assets')));
+app.use('/node_modules',express.static(path.join(__dirname, '/node_modules')));
+app.use('/app',express.static(path.join(__dirname, '/app')));
+app.use('/css',express.static(path.join(__dirname, '/app')));
+app.use('/js',express.static(path.join(__dirname, '/app')));
+app.use('/fonts',express.static(path.join(__dirname, '/app')));
+app.use('/images',express.static(path.join(__dirname, '/app')));
+app.use('/templates',express.static(path.join(__dirname, '/app')));
+
+
+app.get('/*', (req,res)=>{
+
+  res.sendFile(__dirname  + "/index.html");
+
+});
